b00ff2a1d8c8e06978b8f318ccbcf12fc1175d87
Jøran Vagnby Lillesand <lillesand@gmail.com>
2013-09-07 14:23:49 +0200

    Ok, da har vi video

---
+++ busometer/lib/js/webcam.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    var $videos= $('video.webcam');
+    navigator.getMedia(
+        {
+            video: true,
+            audio: false
+        },
+        function(stream) {
+            $videos.each(function(index, video) {
+                var $video = $(video);
+                if (navigator.mozGetUserMedia) {
+                    $video.attr('mozSrcObject', stream);
+                } else {
+                    var vendorURL = window.URL || window.webkitURL;
+                    $video.attr('src', vendorURL.createObjectURL(stream));
+                }
+
+                video.play();
+            });
+        },
+        function(err) {
+            console.log("An error occured! " + err);
+        }
+    );
+
+});
