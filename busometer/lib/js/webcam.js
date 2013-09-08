0cb0207bd845fd937fb6caa4946f173ea3ac48e4
Jøran Vagnby Lillesand <lillesand@gmail.com>
2013-09-08 15:34:50 +0200

    Fixed webcam

--- busometer/lib/js/webcam.js
+++ busometer/lib/js/webcam.js
@@ -20,13 +20,20 @@ $(document).ready(function() {
                     var vendorURL = window.URL || window.webkitURL;
                     $video.attr('src', vendorURL.createObjectURL(stream));
                 }
-
-                video.play();
             });
         },
         function(err) {
             console.log("An error occured! " + err);
         }
     );
+
+    (function() {
+        var $videoElement = $();
+        Reveal.addEventListener('slidechanged', function(e) {
+            if ($videoElement.length == 1) $videoElement.get(0).pause();
+            $videoElement = $(e.currentSlide).find('video');
+            if ($videoElement.length == 1) $videoElement.get(0).play();
+        });
+    })();
 
 });
