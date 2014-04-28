bfdf4d163d281e5d607520de2fb7c3636468388d
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-04-28 10:26:35 +0200

    Some more work on remote cam

--- amazing-web/demos/remote/signalling/signalio.js
+++ amazing-web/demos/remote/signalling/signalio.js
@@ -1,7 +1,9 @@
 var io = require('socket.io').listen(1337);
 
 io.sockets.on('connection', function (socket) {
+    console.log('got connection');
     socket.on('message', function (data) {
+        console.log(data);
         socket.broadcast.emit('message', data);
     });
 });
