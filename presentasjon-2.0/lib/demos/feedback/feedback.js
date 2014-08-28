9bb352a13edbd25c84c1b8052590dbcb0402a29f
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-08-28 23:14:29 +0200

    such nice colores

--- presentasjon-2.0/lib/demos/feedback/feedback.js
+++ presentasjon-2.0/lib/demos/feedback/feedback.js
@@ -3,6 +3,12 @@
     var host = document.querySelector('#service-url').textContent;
     var socket = io.connect(host + ':80');
 
+    var voteMapping = {
+        '1': 'red',
+        '2': 'yellow',
+        '3': 'green'
+    };
+
     socket.on('message', function (data) {
         var messageElement = document.createElement('li');
         messageElement.textContent = data.message;
@@ -16,8 +22,17 @@
     var ticker = document.querySelector('#feedback-ticker');
     socket.on('consumed', function(data) {
         ticker.textContent = data.message;
+        ticker.classList.add(voteMapping[data.vote]);
+
+        var smileyHolder = document.createElement('span');
+        smileyHolder.textContent = data.smiley;
+        smileyHolder.classList.add('smiley');
+        ticker.appendChild(smileyHolder);
 
         setTimeout(function() {
+            ticker.classList.remove('red');
+            ticker.classList.remove('yellow');
+            ticker.classList.remove('green');
             socket.emit('consume');
         }, 6000);
     });
