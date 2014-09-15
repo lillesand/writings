2c347ac2045af7aaf92d345f2dfce945ac7dcf80
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-09-15 17:48:25 +0200

    Restructured code to make it easier to change

--- presentasjon-2.0/lib/demos/speech/speech.js
+++ presentasjon-2.0/lib/demos/speech/speech.js
@@ -1,39 +1,32 @@
 (function() {
 
     var talking = false;
-    var previousSpeechTimer;
-    var changeSlideTimer;
+    var timers;
 
     window.speechSynthesis.onvoiceschanged = function() {
         console.log('Available voices', window.speechSynthesis.getVoices().map(function(voice) { return voice.name; }));
 
         util.section('self-controlling', function(slide) {
             window.speechSynthesis.cancel(); // Cancel any ongoing speech
-            clearTimeout(previousSpeechTimer); // Cancel any pending speech
-            clearTimeout(changeSlideTimer); // Cancel any ongoing slide change
             talking = false;
+            if (timers) {
+                clearTimeout(timers.speech); // Cancel any pending speech
+                clearTimeout(timers.navigation); // Cancel any ongoing slide change
+            }
+
 
             if (!slide) return;
 
             var speechElement = slide.querySelector('.speech');
-            var voice = getVoice(speechElement.dataset.voice);
-            var delayBeforeTalking = speechElement.dataset.initialDelay || 1500;
-            var delayBeforeChangingSlide = speechElement.dataset.navigationDelay || 1500;
-            var sentence = speechElement.textContent;
 
-            previousSpeechTimer = setTimeout(function() {
-                talking = true;
-                speak(voice, sentence, function() {
-                    if (talking) {
-                        // If you finished talking without being canceled (i.e. a new page loaded), navigate to the next slide
-                        changeSlideTimer = setInterval(function() {
-                            Reveal.navigateDown();
-                        }, delayBeforeChangingSlide);
+            if (speechElement) {
+                var voice = getVoice(speechElement.dataset.voice);
+                var delayBeforeTalking = speechElement.dataset.initialDelay || 1500;
+                var delayBeforeChangingSlide = speechElement.dataset.navigationDelay || 1500;
+                var sentence = speechElement.textContent;
 
-                    }
-                    talking = false;
-                });
-            }, delayBeforeTalking);
+                timers = scheduleSpeaking(voice, sentence, delayBeforeChangingSlide, delayBeforeTalking);
+            }
         });
 
         function speak(voice, sentence, then) {
@@ -47,6 +40,28 @@
             setTimeout(function () {
                 window.speechSynthesis.speak(msg);
             }, 0);
+        }
+
+        function scheduleSpeaking(voice, sentence, delayBeforeChangingSlide, delayBeforeTalking) {
+            var speechTimer, navigationTimer;
+            speechTimer = setTimeout(function () {
+                talking = true;
+                speak(voice, sentence, function () {
+                    if (talking) {
+                        // If you finished talking without being canceled (i.e. a new page loaded), navigate to the next slide
+                        navigationTimer = setTimeout(function () {
+                            Reveal.navigateDown();
+                        }, delayBeforeChangingSlide);
+
+                    }
+                    talking = false;
+                });
+            }, delayBeforeTalking);
+
+            return {
+                speech: speechTimer,
+                navigation: navigationTimer
+            };
         }
 
         function getVoice(wanted) {
