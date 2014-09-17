c641fed6df75c4ee3659f09583eb34b1223f5e5b
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-09-17 09:33:13 +0200

    More color show

--- presentasjon-2.0/lib/demos/speech-recognition/speech-recognition.js
+++ presentasjon-2.0/lib/demos/speech-recognition/speech-recognition.js
@@ -12,9 +12,12 @@
             // Let's define a command.
             var commands = {
                 'hello': inSlide(function() { console.log('Hello world!'); }),
-                'color :color': inSlide(function(color) {
+                'background :color': inSlide(function(color) {
                     var slideBackground = document.querySelector('.slide-background.present .slide-background.present');
                     slideBackground.style.backgroundColor = color;
+                }),
+                'color :color': inSlide(function(color) {
+                    currentSlide.querySelector('*').style.color = color;
                 }),
                 'previous slide': inSlide(function() { Reveal.navigateUp(); }),
                 'next slide': inSlide(function() { Reveal.navigateDown(); })
