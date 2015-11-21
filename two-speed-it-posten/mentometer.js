5dbb220369885ddfa72c97439dc7063d858f8fa1
Jøran Vagnby Lillesand <lillesand@gmail.com>
2015-11-21 16:37:01 +0100

    began presentation of two speed it

---
+++ two-speed-it-posten/mentometer.js
@@ -0,0 +1,206 @@
+var questions = [
+    {
+        "question": "Hvem vinner cupfinalen?",
+        "alternatives": [
+            "Rosenborg",
+            "Rosenborg",
+            "Sarpsborg 08",
+            "Rosenborg"
+        ]
+    },
+    {
+        "question": "Hvor lenge har du jobbet i BEKK?",
+        "alternatives": [
+            "1-3 år",
+            "4-6 år",
+            "7-10 år",
+            "En evighet"
+        ]
+    },
+    {
+        "question": "Hva er best?",
+        "alternatives": [
+            "BMC",
+            "Interactive",
+            "Operations",
+            "Hest"
+        ]
+    },
+    {
+        "question": "Hva er det viktigste formålet til en faggruppe?",
+        "alternatives": [
+            "Sikre riktig kompetanse",
+            "Skape faglig engasjement",
+            "Profilere selskapet",
+            "Spise pizza"
+        ]
+    },
+    {
+        "question": "Hva er mest verdsatt?",
+        "alternatives": [
+            "Foredrag på konferanser",
+            "Fagledere",
+            "Spirit",
+            "Innsats på prosjekt"
+        ]
+    },
+    {
+        "question": "Initiativene i BEKK…",
+        "alternatives": [
+            "tar over verden!",
+            "angår ikke meg",
+            "er avhengig av faggruppene"
+        ]
+    },
+    {
+        "question": "Hva er det beste antallet faggruppemøter i løpet av et år?",
+        "alternatives": [
+            "Under 6",
+            "6-9",
+            "10-12",
+            "It depends"
+        ]
+    },
+    {
+        "question": "Hvorfor slutter folk i BEKK?",
+        "alternatives": [
+            "De vet ikke bedre",
+            "Gode alternativer",
+            "Manglende karriereveier",
+            "Dårlige betingelser"
+        ]
+    },
+    {
+        "question": "Å snakke om programmering i lunsjen er…",
+        "alternatives": [
+            "KONGE!",
+            "Nerdete",
+            "Søtt",
+            "Fakturerbart"
+        ]
+    },
+    {
+        "question": "BEKKs største styrke er…",
+        "alternatives": [
+            "Faggruppene",
+            "Tverrfaglighet",
+            "Merkevaren",
+            "Kundene"
+        ]
+    },
+    {
+        "question": "Hvordan skal BEKK vokse videre?",
+        "alternatives": [
+            "Mer av det samme!",
+            "Nye forretningsområder",
+            "Vokse geografisk",
+            "Vi er store nok"
+        ]
+    },
+    {
+        "question": "Samarbeid mellom faggrupper",
+        "alternatives": [
+            "Ruler!",
+            "Nytes i små mengder",
+            "Suger",
+            "Skjer ikke"
+        ]
+    },
+    {
+        "question": "Hva er det beste med å jobbe i BEKK?",
+        "alternatives": [
+            "Prosjektene",
+            "Lønna",
+            "Det sosiale miljøet",
+            "Fagmiljøet"
+        ]
+    },
+    {
+        "question": "Hva er faktureringsmålet?",
+        "alternatives": [
+            "Et mål som skal treffes",
+            "Minimumsverdi",
+            "Maksimumsverdi",
+            "Bare tull"
+        ]
+    },
+    {
+        "question": "Informasjonsflyten i BEKK er…",
+        "alternatives": [
+            "For liten",
+            "Passe",
+            "Altfor mye",
+            "For dårlig"
+        ]
+    },
+    {
+        "question": "Hva er den beste faggruppestørrelsen?",
+        "alternatives": [
+            "Færre enn 7",
+            "8-10",
+            "11-14",
+            "Flere enn 15"
+        ]
+    },
+    {
+        "question": "Fagdager er først og fremst…",
+        "alternatives": [
+            "et avleggs format",
+            "sløsing med penger",
+            "viktig for læring",
+            "viktig sosialt"
+        ]
+    },
+    {
+        "question": "Fagdager er først og fremst…",
+        "alternatives": [
+            "et avleggs format",
+            "sløsing med penger",
+            "viktig for læring",
+            "viktig sosialt"
+        ]
+    },
+    {
+        "question": "Tverrfaglighet i BEKK er…",
+        "alternatives": [
+            "Et konkurransefortrinn",
+            "En myte",
+            "Bare i kompetansebygging"
+        ]
+    },
+    {
+        "question": "Slack er:",
+        "alternatives": [
+            "En revolusjon!",
+            "Bare for de unge",
+            "Bare for nerds",
+            "En boble"
+        ]
+    },
+    {
+        "question": "En fagleders viktigste jobb er å…",
+        "alternatives": [
+            "være et faglig fyrtårn",
+            "bygge andre",
+            "fasilitere",
+            "spre hygge"
+        ]
+    },
+    {
+        "question": "Om 5 år er jeg…",
+        "alternatives": [
+            "Personalansvarlig",
+            "Prosjektleder",
+            "CEO",
+            "Fagleder"
+        ]
+    },
+    {
+        "question": "Å sette retning for faggruppa…",
+        "alternatives": [
+            "Er faglederens ansvar",
+            "Gjøres på første faggruppemøte",
+            "Er ikke så farlig"
+        ]
+    }
+];
